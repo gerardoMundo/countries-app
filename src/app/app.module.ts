6fb573fa6d9fb934e777a9857f47b6e0805d9da1
gerardoMundo <ger.mundo66@gmail.com>
2023-04-19 22:55:57 -0600

    Routing Module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CountriesModule } from './countries/countries.module';
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CountriesModule, SharedModule],
+  imports: [AppRoutingModule, BrowserModule, CountriesModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
 
 @NgModule({
-  declarations: [SidebarComponent],
+  declarations: [SidebarComponent, HomePageComponent, AboutPageComponent],
   imports: [CommonModule],
-  exports: [SidebarComponent],
+  exports: [SidebarComponent, HomePageComponent, AboutPageComponent],
 })
 export class SharedModule {}
